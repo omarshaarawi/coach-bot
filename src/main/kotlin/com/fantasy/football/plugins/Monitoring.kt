46d95c7118f357b6ffda755bf5921114e47037b0
Omar <omar25shaarawi@gmail.com>
2022-09-05 20:29:28 -0500

    try w/o public key

--- src/main/kotlin/com/fantasy/football/plugins/Monitoring.kt
+++ src/main/kotlin/com/fantasy/football/plugins/Monitoring.kt
@@ -1,4 +1,4 @@
-package com.fantasy.football.plugins
+package com.fantasy.football.plugins // ktlint-disable filename
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
@@ -7,6 +7,13 @@ import io.ktor.server.metrics.micrometer.MicrometerMetrics
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics
+import io.micrometer.core.instrument.binder.system.FileDescriptorMetrics
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics
+import io.micrometer.core.instrument.binder.system.UptimeMetrics
 import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 
@@ -15,7 +22,15 @@ fun Application.configureMonitoring() {
 
     install(MicrometerMetrics) {
         registry = appMicrometerRegistry
-        // ...
+        meterBinders = listOf(
+            ClassLoaderMetrics(),
+            JvmMemoryMetrics(),
+            JvmGcMetrics(),
+            ProcessorMetrics(),
+            JvmThreadMetrics(),
+            FileDescriptorMetrics(),
+            UptimeMetrics()
+        )
     }
 
     routing {
